91f20c23b130f276dbd6eac70d6892a817bbc9e5
uwinkler <uwinkler@kodira.de>
2015-08-20 12:02:23 +0200

    Inital commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+platform :ios, '8.0'
+use_frameworks!
+ 
+target 'commons' do
+	#  pod 'Alamofire', '~> 1.2'
+	#  pod 'MBProgressHUD', '~> 0.9.0'
+end
+
+target 'commonsTests' do
+
+end
+
---
+++ commons/commons.h
@@ -0,0 +1,19 @@
+//
+//  commons.h
+//  commons
+//
+//  Created by Ulrich Winkler on 20/08/15.
+//  Copyright (c) 2015 Kodira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for commons.
+FOUNDATION_EXPORT double commonsVersionNumber;
+
+//! Project version string for commons.
+FOUNDATION_EXPORT const unsigned char commonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <commons/PublicHeader.h>
+
+
